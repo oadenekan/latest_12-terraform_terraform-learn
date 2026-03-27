3643ed61829d847983086ddfa826b6a690b1db92
oadenekan <adenekan.jumoke@gmail.com>
2026-03-27 17:16:17 +0000

    automate Docker and Nginx setup using EC2 user data

--- main.tf
+++ main.tf
@@ -111,6 +111,10 @@ resource "aws_instance" "myapp-server" {
   associate_public_ip_address = true
   key_name = aws_key_pair.ssh-key.key_name
 
+  user_data = file("entry-script.sh")
+
+  user_data_replace_on_change = true
+
   tags = {
     Name: "${var.env_prefix}-server"
   }
